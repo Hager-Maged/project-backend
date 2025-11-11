2e296da899c04d6906d0ff312366ea0e74e37e26
KamelMohamed <kamel.mohamed.zidan.22@gmail.com>
2025-11-12 00:24:50 +0200

    .env update

--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ app.use(express.json());
 require("dotenv").config();
 
 const port = process.env.port || 6000; // note : fe el .env el port=6000
-const URL =
-  process.env.DB_URL ||
-  "mongodb+srv://kamelmohamedzidan22_db_user:Qn8UZzwHwaelonY3@bugopedia.rnyjuwi.mongodb.net/?appName=Bugopedia";
+const URL = process.env.DB_URL;
 const auth_routing = require("../route/auth");
 const profile_router = require("../route/profile-routes");
 const home_routes = require("../route/home-routes");
@@ -25,7 +23,7 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/auth", auth_routing);
 app.use("/api/v1/profile", profile_router);
-app.use("/api/v1/home" , home_routes);
+app.use("/api/v1/home", home_routes);
 
 app.use((req, res) =>
   res.status(404).json({ message: "Route not found", data: null })
