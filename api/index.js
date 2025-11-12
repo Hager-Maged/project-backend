bd927e02dc8d7b14cc2d2f10e9a5fe08dfa43fee
MariamYasser227 <yassermariam444@gmail.com>
2025-11-12 18:18:10 +0200

    last sign

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
