e06d89dd32c16f275c939a7f8f53844105ce5d88
Hager-Maged <hajrmajd02@gmail.com>
2025-11-14 08:16:33 +0200

    get my activity

--- route/profile-routes.js
+++ route/profile-routes.js
@@ -4,6 +4,7 @@ const { default: mongoose } = require("mongoose");
 const { deleteBug } = require("../controllers/profile-controllers/delete-bug");
 const { getMyBugs } = require("../controllers/profile-controllers/get-my-bugs");
 const update_bug = require("../controllers/profile-controllers/update-bug");
+const { getMyActivity } = require("../controllers/profile-controllers/get-my-activity");
 
 
 const router = express.Router();
@@ -13,5 +14,7 @@ router.get("/mybugs/:userId", getMyBugs);
 router.delete("/bug/:Id", deleteBug);
 
 router.patch("/bug/:Id" , update_bug);
+
+router.get("/myactivity/:id", getMyActivity);
 
 module.exports = router;
