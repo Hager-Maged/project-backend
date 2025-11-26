564eaba1b917a98b98dd8fa5d9c208889cb32772
JanaMahmoudTamer <janamahmoudtamersamir@gmail.com>
2025-11-26 17:17:16 +0200

    some changes

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ const port = process.env.port || 4000;
 const URL = process.env.DB_URL;
 
 const auth_routing = require("../route/auth");
-const profile_routing = require("../route/profile");
-const homeRoutes = require("../route/home");
+const profile_routes = require("../route/profile-routes");
+const home_routes = require("../route/home-routes");
 
 mongoose
   .connect(URL)
@@ -23,8 +23,8 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.use("/api/v1/auth", auth_routing);
-app.use("/api/v1/profile", profile_routing);
-app.use("/api/v1/home", homeRoutes);
+app.use("/api/v1/profile", profile_routes);
+app.use("/api/v1/home", home_routes);
 
 app.use((req, res) =>
   res.status(404).json({ message: "Route not found", data: null })
